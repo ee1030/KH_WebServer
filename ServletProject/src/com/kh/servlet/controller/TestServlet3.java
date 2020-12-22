22455ee3c182a045561649df1183878841cf7e14
chimchak <sksguswoek1@gmail.com>
2020-12-22 11:18:15 +0900

    update : ServletProject
    비즈니스 로직 추가

--- ServletProject/src/com/kh/servlet/controller/TestServlet3.java
+++ ServletProject/src/com/kh/servlet/controller/TestServlet3.java
@@ -8,52 +8,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class TestServlet2 extends HttpServlet {
+
+public class TestServlet3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public TestServlet2() {
+
+    public TestServlet3() {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// post 방식의 데이터 전달 -> 문자 인코딩이 깨짐 -> UTF-8 형식으로 세팅
 		request.setCharacterEncoding("UTF-8");
-		// POST 방식으로 데이터 전송 시 인코딩이 정해져 있지 않아
-		// 아스키 코드 범위(영어, 숫자, 특수문자 몇개)를 제외한 문자는
-		// 모두 깨진다.
 		
-		// POST 방식으로 데이터 전송 시 문자 인코딩이 지정되지 않아
-		// 전달되는 데이터의 문자 인코딩이 브라우저의 기본값(ISO-8859-1)을 따름.
-		// UTF-8(이클립스 세팅) 과 일치하지 않으므로
-		// 파라미터를 얻어오기 전에 request의 문자 인코딩을 UTF-8로 변경.
-		
-		// 요청 데이터(파라미터)를 모두 얻어와 각 변수에 저장
+		// 전달된 파라미터를 변수에 저장
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
 		String age = request.getParameter("age");
 		String city = request.getParameter("city");
 		String height = request.getParameter("height");
+		
 		String[] foodArr = request.getParameterValues("food");
 		
-		// 파라미터 전달 확인
-		System.out.println("name : " + name);
-		System.out.println("gender : " + gender);
-		System.out.println("age : " + age);
-		System.out.println("city : " + city);
-		System.out.println("height : " + height);
+		// 요청 데이터를 이용해서 새로운 응답 데이터를 생성
+		String gift = null;
 		
-//		for(String food : foodArr) {
-//			System.out.println("food : " + food);
-//		}
-		
-		for(int i=0; i<foodArr.length; i++) {
-			System.out.println("foodArr[" + i + "] : " + foodArr[i]);
+		// 나이대에 따른 선물 추천
+		switch(age) {
+		case "10대 미만" : gift = "슬라임"; break;
+		case "10대" : gift = "닌텐도 스위치"; break;
+		case "20대" : gift = "스마트폰"; break;
+		case "30대" : gift = "돈"; break;
+		case "40대" : gift = "건강"; break;
+		case "50대" : gift = "청춘"; break;
 		}
 		
-		// 응답 화면 준비
-		response.setContentType("text/html; charset=UTF-8");
+		// 응답 화면 출력 준비
+		response.setContentType("text/html; charset=UTF-8"); 
 		
-		// 응답 화면을 내보낼 스트림 연결
+		// 작성된 HTML 문자열을 출력하기 위한 스트림 준비
 		PrintWriter out = response.getWriter();
 		
 		out.println("<!DOCTYPE html>\r\n" + 
@@ -61,7 +55,7 @@ public class TestServlet2 extends HttpServlet {
 				"<head>\r\n" + 
 				"    <meta charset=\"UTF-8\">\r\n" + 
 				"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + 
-				"    <title>TestServlet2 응답 페이지</title>\r\n" + 
+				"    <title>TestServlet3 응답 페이지</title>\r\n" + 
 				"    <style>\r\n" + 
 				"        h1 { color : red }\r\n" + 
 				"        span.name { color : orange }\r\n" + 
@@ -74,7 +68,7 @@ public class TestServlet2 extends HttpServlet {
 				"    </style>\r\n" + 
 				"</head>\r\n" + 
 				"<body>\r\n" + 
-				"    <h1>개인 정보 입력 결과(POST)</h1>");
+				"    <h1>개인 정보 입력 결과(비즈니스 로직)</h1>");
 		
 		out.printf("    <span class='name'>%s</span>님은\r\n" + 
 				"    <span class='age'>%s</span> 이며,\r\n" + 
@@ -83,11 +77,15 @@ public class TestServlet2 extends HttpServlet {
 				"    <span class='gender'>%s</span>입니다.\r\n" + 
 				"    <br>\r\n" + 
 				"    좋아하는 음식은\r\n" + 
-				"    <span class='food'>%s</span> 입니다.\r\n" + 
-				"</body>\r\n" + 
-				"</html>", name, age, city, height, gender,
+				"    <span class='food'>%s</span> 입니다.\r\n"
+				, name, age, city, height, gender,
 						String.join(", ", foodArr));
 		
+		out.println("<h3>" + age + "에 추천할만한 선물</h3>");
+		out.println("<h4>" + gift + " 선물은 어떠신지? </h4>");
+		
+		out.println("</body>" + 
+					"</html>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
